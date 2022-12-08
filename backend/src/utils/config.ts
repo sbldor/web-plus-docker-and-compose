4ff543d83deec8661cec12a281cd33137c4f2397
sbldor <sydor.a@yandex.ru>
2022-12-08 23:29:58 +0300

    ff

--- backend/src/utils/config.ts
+++ backend/src/utils/config.ts
@@ -1,13 +1,11 @@
-const dotenv = require('dotenv');
-
-dotenv.config({ path: '../../.env' });
+require('dotenv').config({ path: '../../.env' });
 
 export default () => ({
   jwtSecret: process.env.JWTSECRET,
   YANDEX_CLIENT_ID: process.env.YANDEX_CLIENT_ID,
   YANDEX_CLIENT_SECRET: process.env.YANDEX_CLIENT_SECRET,
   YANDEX_REDIRECT_URI: process.env.YANDEX_REDIRECT_URI,
-  port: parseInt(process.env.PORT) || 3001,
+  port: parseInt(process.env.PORT) || 3000,
   db: {
     database: process.env.POSTGRES_DB,
     host: process.env.POSTGRES_HOST,
